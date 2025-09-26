bbf41f0cc603d6ca8b768780d7d40b770faf88f6
Rahul Gangwar <rahulgangwar2625@gmail.com>
2025-09-26 23:03:59 +0530

    redux-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Navbar'
+import Shop from './Shop'
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <div className='container'>
+      <Shop/>
+    </div>
+    </>
+  )
+}
+
+export default App
